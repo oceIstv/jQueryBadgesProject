365b8d3582eb098b56ca682ab8be105bc297eda2
István Huszár <istvan.huszar.93@gmail.com>
2017-07-25 08:59:47 +0200

    Full

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/osrh.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response);
+    addCourses(response.courses.completed);
+    }
+  });
+
+
+
+  var addCourses = function(courses){
+    var badges = $('#badges');
+    courses.forEach(function(course){
+
+      var $course = $('<div />',{
+        'class' : 'course'
+      }).appendTo(badges);
+
+      $('<h3 />',{
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />',{
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href:course.url,
+        text: 'Click for Course'
+      }).appendTo($course);
+    })
+  };
+
 
 });
